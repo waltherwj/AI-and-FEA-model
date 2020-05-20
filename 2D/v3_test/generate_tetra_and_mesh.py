79be2c3143b46e0bb0d636fc9b2126ebdcb1b9c1
saatsin <walther.johnson@ucalgary.ca>
2020-05-20 13:12:10 -0600

    test: attempt to set version to spaceclaim API

--- 2D/v3_test/generate_tetra_and_mesh.py
+++ 2D/v3_test/generate_tetra_and_mesh.py
@@ -3,7 +3,7 @@ import random
 import math
 from itertools import combinations, product, izip
 from System import Random
-SpaceClaim.Api.V19.Api
+#SpaceClaim.Api.V19.Api
 n_points = 3
 n_dimensions = 3
 points = []
@@ -224,7 +224,7 @@ if n_points > 3:
 
 
 # Create/Edit Mesh
-options = CreateMeshOptions()
+options = SpaceClaim.Api.V18.Scripting.Commands.CommandOptions.CreateMeshOptions()
 options.SolidElementShape = ElementShapeType.Hexahedral
 options.SurfaceElementShape = ElementShapeType.QuadDominant
 options.BlockingType = BlockingDecompositionType.Aggressive
