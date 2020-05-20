050ad6c9c733d85b380026ddc0f0182e4d37dba3
saatsin <walther.johnson@ucalgary.ca>
2020-05-20 13:34:52 -0600

    feat: add v4 files and folder

---
+++ 2D/v4_test/WORKING_script.py
@@ -0,0 +1,10 @@
+﻿# encoding: utf-8
+# 2020 R1
+SetScriptVersion(Version="20.1.164")
+system1 = GetSystem(Name="SYS")
+geometry1 = system1.GetContainer(ComponentName="Geometry")
+DSscript = open("D:/Ansys Simulations/Project/2D/v3_test/generate_tetra_and_mesh.py", "r")
+DSscriptcommand=DSscript.read()
+geometry1.SendCommand(Command=DSscriptcommand,Language="Python")
+geometry1.Edit(IsSpaceClaimGeometry=True)
+#geometry1.Exit()
