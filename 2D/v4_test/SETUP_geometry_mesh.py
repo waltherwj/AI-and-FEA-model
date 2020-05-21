b157cfd058aa7c54f2e1ac17ce75b51d1c35f656
saatsin <walther.johnson@ucalgary.ca>
2020-05-20 21:42:24 -0600

    fix: correct file name for generating geometry

--- 2D/v4_test/SETUP_geometry_mesh.py
+++ 2D/v4_test/SETUP_geometry_mesh.py
@@ -3,7 +3,7 @@
 SetScriptVersion(Version="20.1.164")
 system1 = GetSystem(Name="SYS")
 geometry1 = system1.GetContainer(ComponentName="Geometry")
-DSscript = open("D:/Ansys Simulations/Project/2D/v4_test/MESH_generate.py", "r")
+DSscript = open("D:/Ansys Simulations/Project/2D/v4_test/GEOMETRY_generate.py", "r")
 DSscriptcommand=DSscript.read()
 geometry1.SendCommand(Command=DSscriptcommand,Language="Python")
 geometry1.Edit(IsSpaceClaimGeometry=True)
