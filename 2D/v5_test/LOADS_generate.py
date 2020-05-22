21fc336c0a8acc65c21787c0d2b93b6425c87e7e
saatsin <walther.johnson@ucalgary.ca>
2020-05-22 16:07:39 -0600

    feat: select nodes to apply displacement to

--- 2D/v5_test/LOADS_generate.py
+++ 2D/v5_test/LOADS_generate.py
@@ -1,25 +1,38 @@
 import random
 import itertools
-    ## Load in the body
+    ## Load-in the body
     
 part = Model.Geometry.Children[0] #Get first "geometry"
 body = part.Children[0].GetGeoBody() #Get geometrical entities of first body
 selection = ExtAPI.SelectionManager.CreateSelectionInfo(SelectionTypeEnum.GeometryEntities)  # Create an empty selection.
 
+    ## General Analysis Settings
+settings = Model.Analyses[0].AnalysisSettings
+settings.LargeDeflection = True
+settings.WeakSprings = WeakSpringsType.ProgramControlled
     ## Create vector conditions
     
 n_vertices = len(body.Vertices)
 displaced_vertices = random.choice(range(0, n_vertices+1)) #number of vertices w/ a boundary displacement condition
 choose_vertices = []
-vert_chosen = [1] * displaced_vertices
-vert_not_chosen = [0] * (n_vertices-displaced_vertices)
+vert_chosen = [True] * displaced_vertices
+vert_not_chosen = [False] * (n_vertices-displaced_vertices)
 choose_vertices = vert_chosen + vert_not_chosen
 random.shuffle(choose_vertices) #shuffles the list to not always choose the first vertices
 
+    ## Delete displacements from previous analysis
+analysis = Model.Analyses[0]
+for disp in Model.Analyses[0].GetChildren(DataModelObjectCategory.Displacement, False):
+    pass
+displacements = []
+ii=0
 for i, vertex in enumerate(body.Vertices):
-    if choose_vertices[i] = 1:
+    if choose_vertices[i]:
         selection.Entities = [vertex]
-        Model.Analyses[0].AddDs
+        displacements.append(Model.Analyses[0].AddDisplacement())
+        displacements[ii].Location = selection
+        ii += 1
+        
     
 ## Create list of edges
 
