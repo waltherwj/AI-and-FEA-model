f72b0139294d1b9cdef82923ce48dbc8025bed49
saatsin <walther.johnson@ucalgary.ca>
2020-05-23 17:53:36 -0600

    test: try to do error handling from workbench

--- 2D/v6_test/MESH_error_handling.py
+++ 2D/v6_test/MESH_error_handling.py
@@ -1,8 +1,3 @@
-
-try:
-    DSscript = open("D:/Ansys Simulations/Project/2D/v6_test/MESH_generate.py", "r")
-    DSscriptcommand=DSscript.read()
-    model1.SendCommand(Command=DSscriptcommand,Language="Python")
-except:
-    mesh_method.Method = MethodType.Automatic
-    Model.Mesh.GenerateMesh()
+#helper file to handle if mesh can't be generated with hexa components
+mesh_method.Method = MethodType.Automatic
+Model.Mesh.GenerateMesh()
