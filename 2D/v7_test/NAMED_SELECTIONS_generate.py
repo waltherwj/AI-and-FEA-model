9bfdfe58054d1084a5ebb3e8bf37ee24f6345dfd
saatsin <walther.johnson@ucalgary.ca>
2020-05-24 18:37:30 -0600

    feat: add script that shows how to create worksheet and criterion

--- 2D/v7_test/NAMED_SELECTIONS_generate.py
+++ 2D/v7_test/NAMED_SELECTIONS_generate.py
@@ -1,1 +1,6 @@
+## Script to generate named selections for forces and displacements
 
+nodes2 = Model.NamedSelections.Children[4]
+nodes2.ScopingMethod = GeometryDefineByType.Worksheet
+criterion = Ansys.ACT.Automation.Mechanical.NamedSelectionCriterion
+nodes2.GenerationCriteria.Add(criterion(actionType = SelectionActionType.Add)) ##creates new selection criterion
