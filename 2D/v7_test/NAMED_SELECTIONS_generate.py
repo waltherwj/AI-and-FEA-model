f4cab3468b0591daaad95e5d993cf25e1297e786
saatsin <walther.johnson@ucalgary.ca>
2020-05-25 16:42:27 -0600

    feat: add bounding box section

--- 2D/v7_test/NAMED_SELECTIONS_generate.py
+++ 2D/v7_test/NAMED_SELECTIONS_generate.py
@@ -1,3 +1,4 @@
+import math
 ## Script to generate named selections for forces and displacements
 
 ##Delete Previous NS
@@ -5,10 +6,11 @@ for named_selection in Model.NamedSelections.Children:
     named_selection.Delete()
 
 ## Gets bounding box
-body = Model.Geometry.Children[0]
-X = body.LengthX
-Y = body.LengthY
-Z = body.LengthZ
+part = Model.Geometry.Children[0]
+body = part.Children[0]
+geobody = body.GetGeoBody()
+x1,y1,z1,x2,y2,z2 = geobody.GetBoundingBox() # [x1 y1 z1 x2 y2 z2] coordinates of the bounding box, lower to higher
+
 
 ##Creates named selections
 Model.AddNamedSelection()  #Adds a named selection
@@ -27,6 +29,4 @@ ns.GenerationCriteria.Add(criterion()) ##creates new empty selection criterion
 #criterion(actionType = SelectionActionType.Add)
 ns.GenerationCriteria[0].EntityType = SelectionType.MeshNode 
 
-for i,item in enumerate(SelectionActionType):
-    print(item)
 
