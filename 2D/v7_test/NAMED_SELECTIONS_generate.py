ab5ebb99fb8181eef4c58968c0b906d54cf0b5d5
saatsin <walther.johnson@ucalgary.ca>
2020-05-25 19:07:10 -0600

    feat: add criteria generation

--- 2D/v7_test/NAMED_SELECTIONS_generate.py
+++ 2D/v7_test/NAMED_SELECTIONS_generate.py
@@ -11,12 +11,6 @@ body = part.Children[0]
 geobody = body.GetGeoBody()
 x1,y1,z1,x2,y2,z2 = geobody.GetBoundingBox() # [x1 y1 z1 x2 y2 z2] coordinates of the bounding box, lower to higher
 
-
-##Creates named selections
-Model.AddNamedSelection()  #Adds a named selection
-number_of_ns = len( Model.NamedSelections.Children)
-ns = Model.NamedSelections.Children[number_of_ns-1] #creates a temporary variable to store it
-ns.ScopingMethod = GeometryDefineByType.Worksheet #changes the scoping method to be worksheet
 ## creates a criterion object and stores it
 criterion = Ansys.ACT.Automation.Mechanical.NamedSelectionCriterion
 #Criterion Options: active: bool, actionType: SelectionActionType, 
@@ -25,8 +19,21 @@ criterion = Ansys.ACT.Automation.Mechanical.NamedSelectionCriterion
 #lowerBound: Quantity, upperBound: Quantity, 
 #coordinateSystem: CoordinateSystem
 
-ns.GenerationCriteria.Add(criterion()) ##creates new empty selection criterion
-#criterion(actionType = SelectionActionType.Add)
-ns.GenerationCriteria[0].EntityType = SelectionType.MeshNode 
+##Choose Number of forces
+##Choose number of displacements
 
+##Creates named selections
+
+number_of_selections = 3
+for i in range(number_of_selections):
+    Model.AddNamedSelection()  #Adds a named selection
+    number_of_ns = len( Model.NamedSelections.Children)
+    ns = Model.NamedSelections.Children[number_of_ns-1] #creates a temporary variable to store it
+    ns.ScopingMethod = GeometryDefineByType.Worksheet #changes the scoping method to be worksheet
+    number_of_criteria = 3
+    for i in range(number_of_criteria):
+        ns.GenerationCriteria.Add(criterion()) ##creates new empty selection criterion
+        ns.GenerationCriteria[0].EntityType = SelectionType.MeshNode 
+
+    ns.Generate()
 
