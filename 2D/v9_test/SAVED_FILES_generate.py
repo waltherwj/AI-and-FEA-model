21952108932e98233f0cfcbb84829caacee5d864
saatsin <walther.johnson@ucalgary.ca>
2020-05-28 16:38:51 -0600

    feat: add code to handle amount of directional deformations

--- 2D/v9_test/SAVED_FILES_generate.py
+++ 2D/v9_test/SAVED_FILES_generate.py
@@ -1,1 +1,21 @@
+## Creates solutions and exports them
 
+## Load-in the body
+part = Model.Geometry.Children[0] #Get first "geometry"
+body = part.Children[0] #Get  first body
+geobody = body.GetGeoBody()
+#selection = ExtAPI.SelectionManager.CreateSelectionInfo(SelectionTypeEnum.GeometryEntities)  # Create an empty selection.
+
+
+## Delete previously assigned forces
+analysis = Model.Analyses[0]
+solution = analysis.Solution
+deformation = []
+number_directional_deformations = len(solution.GetChildren(DataModelObjectCategory.DirectionalDeformation, False))
+while number_directional_deformations != 3:
+    if number_directional_deformations < 3:
+        number_directional_deformations =len(solution.GetChildren(DataModelObjectCategory.DirectionalDeformation, False))
+        deformation.append(solution.AddDirectionalDeformation())
+    if number_directional_deformations > 3:
+        number_directional_deformations =len(solution.GetChildren(DataModelObjectCategory.DirectionalDeformation, False))
+        solution.Children[1].Delete()
