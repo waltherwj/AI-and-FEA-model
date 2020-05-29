f8511c3c0898e2e5da8f238f4dd35228507aa824
saatsin <walther.johnson@ucalgary.ca>
2020-05-29 14:01:33 -0600

    feat: create v10 folder

---
+++ 2D/v10_test/SAVED_FILES_generate.py
@@ -0,0 +1,43 @@
+## Creates solutions and exports them
+
+## Load-in the body
+part = Model.Geometry.Children[0] #Get first "geometry"
+body = part.Children[0] #Get  first body
+geobody = body.GetGeoBody()
+#selection = ExtAPI.SelectionManager.CreateSelectionInfo(SelectionTypeEnum.GeometryEntities)  # Create an empty selection.
+
+
+## Delete previously assigned forces
+analysis = Model.Analyses[0]
+solution = analysis.Solution
+"""
+number_directional_deformations = len(solution.GetChildren(DataModelObjectCategory.DirectionalDeformation, False))
+while number_directional_deformations != 3:
+    number_directional_deformations =len(solution.GetChildren(DataModelObjectCategory.DirectionalDeformation, False))
+    if number_directional_deformations < 3:
+        solution.AddDirectionalDeformation()
+    if number_directional_deformations > 3:
+        solution.Children[number_directional_deformations].Delete()
+## Set Directions
+deformationX = solution.Children[1]
+deformationY = solution.Children[2]
+deformationZ = solution.Children[3]
+
+deformationX.NormalOrientation = NormalOrientationType.XAxis
+deformationY.NormalOrientation = NormalOrientationType.YAxis
+deformationZ.NormalOrientation = NormalOrientationType.ZAxis
+"""
+number_udr = len(solution.GetChildren(DataModelObjectCategory.UserDefinedResult, False))
+if number_udr < 1:
+    solution.AddUserDefinedResult()
+number_udr = len(solution.GetChildren(DataModelObjectCategory.UserDefinedResult, False))
+udr = solution.Children[number_udr]
+udr.Expression = 'UVECTORS'
+##
+"""
+nodal.XComponent.Output.DiscreteValues[0].Value
+
+"""
+
+## Solves the model
+Model.Solve()
---
+++ 2D/v10_test/SETUP_all.py
@@ -0,0 +1,38 @@
+﻿# encoding: utf-8
+# 2020 R1
+
+## Create Geometry
+SetScriptVersion(Version="20.1.164")
+system1 = GetSystem(Name="SYS")
+geometry1 = system1.GetContainer(ComponentName="Geometry")
+DSscript = open("D:/Ansys Simulations/Project/2D/v10_test/GEOMETRY_generate.py", "r")
+DSscriptcommand=DSscript.read()
+geometry1.SendCommand(Command=DSscriptcommand,Language="Python")
+geometry1.Edit(IsSpaceClaimGeometry=True)
+#geometry1.Exit()
+
+## Open Mechanical
+modelComponent1 = system1.GetComponent(Name="Model")
+modelComponent1.Refresh()
+model1 = system1.GetContainer(ComponentName="Model")
+model1.Edit()
+
+## Create Mesh
+DSscript = open("D:/Ansys Simulations/Project/2D/v10_test/MESH_generate.py", "r")
+DSscriptcommand=DSscript.read()
+model1.SendCommand(Command=DSscriptcommand,Language="Python")
+
+## Create Named  Selections
+DSscript = open("D:/Ansys Simulations/Project/2D/v10_test/NAMED_SELECTIONS_generate.py", "r")
+DSscriptcommand=DSscript.read()
+model1.SendCommand(Command=DSscriptcommand,Language="Python")
+
+## Create Displacements
+DSscript = open("D:/Ansys Simulations/Project/2D/v10_test/DISPLACEMENTS_generate.py", "r")
+DSscriptcommand=DSscript.read()
+model1.SendCommand(Command=DSscriptcommand,Language="Python")
+
+## Create Loads
+DSscript = open("D:/Ansys Simulations/Project/2D/v10_test/LOADS_generate.py", "r")
+DSscriptcommand=DSscript.read()
+model1.SendCommand(Command=DSscriptcommand,Language="Python")
