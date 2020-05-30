203406a8d0107273c8ce09a15a6421e81024a3c7
saatsin <walther.johnson@ucalgary.ca>
2020-05-29 18:58:35 -0600

    feat: add log file creation and log folder creation exception

--- 2D/v10_test/SAVED_FILES_generate.py
+++ 2D/v10_test/SAVED_FILES_generate.py
@@ -29,17 +29,28 @@ nodal.XComponent.Output.DiscreteValues[0].Value
 Model.Solve()
 
 ## Create folders to store solutions
-sample_number=1
+sample_number=2
 directory = "test_data_dir_" + sample_number.ToString()
 parent_dir = "D:\\Ansys Simulations\\Project\\2D\\v10_test"
 path = os.path.join(parent_dir, directory)
 try:
     os.mkdir(path)
 except:
-    pass
+    log_file = "log.txt"
+    log_file_path = os.path.join(path + '\\..', log_file)
+    f_log= open(log_file_path ,"w+")
+    f_log.write("Folder for sample # " + sample_number.ToString() + " already exists -- OVERWRITING")
+    f_log.close()
 
 ##Create files in this directory to store displacements and forces
 filename = "test_"+ sample_number.ToString() +".txt"
 file_path = os.path.join(path, filename)
 f= open(file_path ,"w+")
+
+##Write in file
+for i in range(10):
+     f.write("This is line %d\r\n" % (i+2))
+     
+## Close file
+f.close() 
 
